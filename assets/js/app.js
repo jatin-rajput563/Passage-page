5de05a0accc91ab11570c2af82c24be717622113
jatin-rajput563 <jkt3298@gmail.com>
2025-02-24 16:44:01 +0530

    modify-section-1

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,8 @@ const navbarbtn = () => {
     document.getElementById("transparent").classList.toggle("bg-transparent")
     document.getElementById("rotats2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
-}
+};
+
 $('.slider-content').slick({
     infinite: true,
     autoplay: true,
@@ -13,6 +14,7 @@ $('.slider-content').slick({
     prevArrow: false,
     nextArrow: false,
 });
+
 let backToTopButton = document.getElementById("backToTop");
 window.onscroll = function () {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
